731c2aa54b532973a8fdf72e3808c37041aac9b3
mohamedmahmoud97 <mohamedmorsy8438@gmail.com>
2018-04-07 01:58:11 +0200

    added script files for running server and client each at a separate terminal with specific algo.

---
+++ sr.sh
@@ -0,0 +1,2 @@
+xfce4-terminal -e 'go run ./examples/server/server.go sr';
+xfce4-terminal -e 'go run ./examples/client/client.go';
---
+++ sw.sh
@@ -0,0 +1,2 @@
+xfce4-terminal -e 'go run ./examples/server/server.go sw';
+xfce4-terminal -e 'go run ./examples/client/client.go';
